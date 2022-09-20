49b932f6aa7251c3a8aa85795769f46fe7a86ef2
jsaarela1 <jsaarela1@yahoo.com>
2022-09-19 20:53:28 -0400

    PLC project lex

---
+++ src/main/java/plc/project/Lexer.java
@@ -0,0 +1,187 @@
+package plc.project;
+
+import java.util.List;
+
+/**
+ * The lexer works through three main functions:
+ *
+ *  - {@link #lex()}, which repeatedly calls lexToken() and skips whitespace
+ *  - {@link #lexToken()}, which lexes the next token
+ *  - {@link CharStream}, which manages the state of the lexer and literals
+ *
+ * If the lexer fails to parse something (such as an unterminated string) you
+ * should throw a {@link ParseException} with an index at the character which is
+ * invalid.
+ *
+ * The {@link #peek(String...)} and {@link #match(String...)} functions are * helpers you need to use, they will make the implementation a lot easier. */
+public final class Lexer {
+
+    private final CharStream chars;
+
+    public Lexer(String input) {
+        chars = new CharStream(input);
+    }
+
+    /**
+     * Repeatedly lexes the input using {@link #lexToken()}, also skipping over
+     * whitespace where appropriate.
+     */
+    public List<Token> lex() {
+        throw new UnsupportedOperationException(); //TODO
+    }
+
+    /**
+     * This method determines the type of the next token, delegating to the
+     * appropriate lex method. As such, it is best for this method to not change
+     * the state of the char stream (thus, use peek not match).
+     *
+     * The next character should start a valid token since whitespace is handled
+     * by {@link #lex()}
+     */
+    public Token lexToken() {
+        if (peek("[A-Za-z]|[@]")) {
+            lexIdentifier();
+        } else if (peek("[0]|[-]|[1-9]")) {
+            lexNumber(); // figure out how to differentiate integer vs a decimal
+        } else if (peek("[']")) {
+            lexCharacter();
+        } else if (peek("\"")) {
+            lexString();
+        } else if (peek("\\")) {
+            lexEscape();
+        } else if (peek(" [!=]|[=]|[&]|[|]")) {
+            lexOperator();
+        } else if (peek(" [\b]|[\n]|[\r]|[\t]")) {
+            // whitespace, thus
+            chars.advance();
+        } else {
+            throw new UnsupportedOperationException(); //TODO
+        }
+        // also need to check for whitespace
+        throw new UnsupportedOperationException();
+    }
+
+    public Token lexIdentifier() {
+        int start = chars.index;
+        int current = start;
+        match("[A-Za-z]|[@]");
+        current++;
+        while (match("[A-Za-z0-9_-]")) {
+            current++;
+        }
+        System.out.println("Identifier of length: " + (current-start));
+        // create token of length current - start!
+        throw new UnsupportedOperationException(); //TODO
+    }
+
+    public Token lexNumber() {
+        System.out.println("number");
+        throw new UnsupportedOperationException(); //TODO
+    }
+
+    public Token lexCharacter() {
+        int start = chars.index;
+        int current = start;
+        match("[']");
+        current++;
+        if (match("[\\]")) {
+            current++;
+            if (match("[']")) {
+                current++;
+            } else
+                throw new ParseException("Invalid character at index ", current);
+        } else
+            throw new ParseException("Invalid character at index ", current);
+        System.out.println("Character of length: " + (current-start));
+        throw new UnsupportedOperationException(); //TODO
+    }
+
+    public Token lexString() {
+        System.out.println("string");
+        throw new UnsupportedOperationException(); //TODO
+    }
+
+    public void lexEscape() {
+        System.out.println("escape");
+        throw new UnsupportedOperationException(); //TODO
+    }
+
+    public Token lexOperator() {
+        System.out.println("operator");
+        throw new UnsupportedOperationException(); //TODO
+    }
+
+    /**
+     * Returns true if the next sequence of characters match the given patterns,
+     * which should be a regex. For example, {@code peek("a", "b", "c")} would
+     * return true if the next characters are {@code 'a', 'b', 'c'}.
+     */
+    public boolean peek(String... patterns) {
+        for (int i = 0; i < patterns.length; i++) {
+            if (!chars.has(i) || !String.valueOf(chars.get(i)).matches(patterns[i]))
+                return false;
+        }
+        return true;
+        //throw new UnsupportedOperationException(); //TODO (in Lecture)
+    }
+
+    /**
+     * Returns true in the same way as {@link #peek(String...)}, but also
+     * advances the character stream past all matched characters if peek returns
+     * true. Hint - it's easiest to have this method simply call peek.
+     */
+    public boolean match(String... patterns) {
+        boolean peek = peek(patterns);
+        if (peek) {
+            for (int i = 0; i < patterns.length; i++) {
+                chars.advance();
+            }
+        }
+        return peek;
+        //throw new UnsupportedOperationException(); //TODO (in Lecture)
+    }
+
+    /**
+     * A helper class maintaining the input string, current index of the char
+     * stream, and the current length of the token being matched.
+     *
+     * You should rely on peek/match for state management in nearly all cases.
+     * The only field you need to access is {@link #index} for any {@link
+     * ParseException} which is thrown.
+     */
+    public static final class CharStream {
+
+        private final String input;
+        private int index = 0;
+        private int length = 0;
+
+        public CharStream(String input) {
+            this.input = input;
+        }
+
+        public boolean has(int offset) {
+            return index + offset < input.length();
+        }
+
+        public char get(int offset) {
+            return input.charAt(index + offset);
+        }
+
+        public void advance() {
+            index++;
+            length++;
+        }
+
+        public void skip() {
+            length = 0;
+        }
+
+        public Token emit(Token.Type type) {
+            int start = index - length;
+            skip();
+            return new Token(type, input.substring(start, index), start);
+        }
+
+    }
+
+}
---
+++ src/test/java/plc/project/LexerTests.java
@@ -0,0 +1,226 @@
+package plc.project;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class LexerTests {
+
+    @ParameterizedTest
+    @MethodSource
+    void testIdentifier(String test, String input, boolean success) {
+        test(input, Token.Type.IDENTIFIER, success);
+    }
+    // regex for testIdentifier
+    // ^([A-Za-z]|[@])([A-Za-z0-9_-])*$
+    private static Stream<Arguments> testIdentifier() {
+        return Stream.of(
+                Arguments.of("Alphabetic", "getName", true),
+                Arguments.of("Alphanumeric", "thelegend27", true),
+                Arguments.of("Contains underscore", "go_gators", true),
+                Arguments.of("Contains hyphen", "lets-go", true),
+                Arguments.of("Starts with an @", "@captainHook", true),
+                Arguments.of("Leading Hyphen", "-five", false),
+                Arguments.of("Leading Digit", "1fish2fish3fishbluefish", false),
+                Arguments.of("Leading Underscore", "_goodTry", false),
+                Arguments.of("Contains @ in middle", "this@stinks", false),
+                Arguments.of("Has @ throughout", "@1@2@3@4time", false)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void testInteger(String test, String input, boolean success) {
+        test(input, Token.Type.INTEGER, success);
+    }
+
+    private static Stream<Arguments> testInteger() {
+        return Stream.of(
+                Arguments.of("Single Digit", "1", true),
+                Arguments.of("Multiple Digits", "12345", true),
+                Arguments.of("Trailing Zeros", "1230000", true),
+                Arguments.of("Zero", "0", true),
+                Arguments.of("Negative", "-1", true),
+                Arguments.of("Negative with 0", "-0", false),
+                Arguments.of("Leading Zero", "01", false)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void testDecimal(String test, String input, boolean success) {
+        test(input, Token.Type.DECIMAL, success);
+    }
+
+    private static Stream<Arguments> testDecimal() {
+        return Stream.of(
+                Arguments.of("Multiple Digits", "123.456", true),
+                Arguments.of("Decimal less than 1", "0.42", true),
+                Arguments.of("Negative Decimal", "-1.0", true),
+                Arguments.of("Trailing Zeros", "5.200000", true),
+                Arguments.of("No decimal point", "45", false),
+                Arguments.of("Trailing Decimal", "1.", false),
+                Arguments.of("Leading zero", "04.12", false),
+                Arguments.of("Leading Decimal", ".5", false)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void testCharacter(String test, String input, boolean success) {
+        test(input, Token.Type.CHARACTER, success);
+    }
+
+    private static Stream<Arguments> testCharacter() {
+        return Stream.of(
+                Arguments.of("Alphabetic", "\'c\'", true),
+                Arguments.of("Newline Escape", "\'\\n\'", true),
+                Arguments.of("Slash", "\'\\\'", true),
+                Arguments.of("Empty", "\'\'", false),
+                Arguments.of("Multiple", "\'abc\'", false)
+        );
+    }
+// made tests up to here
+// still needs to make more unit tests for testString and testOperator
+    @ParameterizedTest
+    @MethodSource
+    void testString(String test, String input, boolean success) {
+        test(input, Token.Type.STRING, success);
+    }
+
+    private static Stream<Arguments> testString() {
+        return Stream.of(
+                Arguments.of("Empty", "\"\"", true),
+                Arguments.of("Alphabetic", "\"abc\"", true),
+                Arguments.of("Newline Escape", "\"Hello,\\nWorld\"", true),
+                Arguments.of("Unterminated", "\"unterminated", false),
+                Arguments.of("Invalid Escape", "\"invalid\\escape\"", false)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void testOperator(String test, String input, boolean success) {
+        //this test requires our lex() method, since that's where whitespace is handled.
+        test(input, Arrays.asList(new Token(Token.Type.OPERATOR, input, 0)), success);
+    }
+
+    private static Stream<Arguments> testOperator() {
+        return Stream.of(
+                Arguments.of("Character", "(", true),
+                Arguments.of("Comparison", "!=", true),
+                Arguments.of("Space", " ", false),
+                Arguments.of("Tab", "\t", false)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void testExamples(String test, String input, List<Token> expected) {
+        test(input, expected, true);
+    }
+
+    private static Stream<Arguments> testExamples() {
+        return Stream.of(
+                Arguments.of("Example 1", "LET x = 5;", Arrays.asList(
+                        new Token(Token.Type.IDENTIFIER, "LET", 0),
+                        new Token(Token.Type.IDENTIFIER, "x", 4),
+                        new Token(Token.Type.OPERATOR, "=", 6),
+                        new Token(Token.Type.INTEGER, "5", 8),
+                        new Token(Token.Type.OPERATOR, ";", 9)
+                )),
+                Arguments.of("Example 2", "print(\"Hello, World!\");", Arrays.asList(
+                        new Token(Token.Type.IDENTIFIER, "print", 0),
+                        new Token(Token.Type.OPERATOR, "(", 5),
+                        new Token(Token.Type.STRING, "\"Hello, World!\"", 6),
+                        new Token(Token.Type.OPERATOR, ")", 21),
+                        new Token(Token.Type.OPERATOR, ";", 22)
+                ))
+        );
+    }
+
+    @Test
+    void testException() {
+        ParseException exception = Assertions.assertThrows(ParseException.class,
+                () -> new Lexer("\"unterminated").lex());
+        Assertions.assertEquals(13, exception.getIndex());
+    }
+
+    /**
+     * Tests that lexing the input through {@link Lexer#lexToken()} produces a
+     * single token with the expected type and literal matching the input.
+     */
+    private static void test(String input, Token.Type expected, boolean success) {
+        try {
+            if (success) {
+                Assertions.assertEquals(new Token(expected, input, 0), new Lexer(input).lexToken());
+            } else {
+                Assertions.assertNotEquals(new Token(expected, input, 0), new Lexer(input).lexToken());
+            }
+        } catch (ParseException e) {
+            Assertions.assertFalse(success, e.getMessage());
+        }
+    }
+
+    /**
+     * Tests that lexing the input through {@link Lexer#lex()} matches the
+     * expected token list.
+     */
+    private static void test(String input, List<Token> expected, boolean success) {
+        try {
+            if (success) {
+                Assertions.assertEquals(expected, new Lexer(input).lex());
+            } else {
+                Assertions.assertNotEquals(expected, new Lexer(input).lex());
+            }
+        } catch (ParseException e) {
+            Assertions.assertFalse(success, e.getMessage());
+        }
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void testPeekSingleChar(String test, String source, boolean success, String pattern) {
+        Assertions.assertEquals(success, new Lexer(source).peek(pattern));
+    }
+
+    private static Stream<Arguments> testPeekSingleChar() {
+        return Stream.of(
+                Arguments.of("first char 0", "012", true, "0"),
+                Arguments.of("first char 1", "012", false, "1"),
+                Arguments.of("first char not digit", "3", true, "\\d")
+        );
+    }
+
+    @Test
+    void testPeekMultiCharTrue() {
+        Assertions.assertTrue(new Lexer("4123420").peek("4", "1", "2"));
+    }
+
+    @Test
+    void testPeekMultiCharFalse() {
+        Assertions.assertFalse(new Lexer("12345").peek("1", "2", "2", "3", "4", "7"));
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void testMatch(String test, String source, boolean success, String... patterns) {
+        Assertions.assertEquals(success, new Lexer(source).match(patterns));
+    }
+
+    private static Stream<Arguments> testMatch() {
+        String strings[] = {"c","="};
+        return Stream.of(
+                Arguments.of("Testing 2", "dc=", true, strings)
+        );
+    }
+
+
+
+}
